74647a0f9346138899bf7dd18e2c77b10bb5a791
feralieva <t.feralieva@gmail.com>
2014-03-23 08:15:10 +0200

    concat files

---
+++ concat_files/solution.py
@@ -0,0 +1,17 @@
+import sys
+
+def main():
+	arr = []
+	list_arg = list(sys.argv)
+	filename = "MEGATRON.txt"
+	file = open(filename,"a")
+	for i in list_arg[1:]:
+		file_name = i
+		file1 = open(file_name,"r")
+		content = file1.read()
+		arr += [content]
+	file.write("\n\n".join(arr))
+	print(arr)
+
+if __name__ == '__main__':
+	main()
---
+++ concat_files/test.py
@@ -0,0 +1,13 @@
+import unittest
+import solution
+from subprocess import call
+from subprocess import check_output
+
+class TestConcatFiles(unittest.TestCase):
+	"""docstring for TestConcatFiles"""
+	def test_concat_files(self):
+		concat = call("py solution.py file1.txt file2.txt",shell=True)
+		check_concat = check_output("cat MEGATRON.txt",shell=True).decode()
+		self.assertEqual('Python is an awesome language!\nYou should try it.\n\nAlso, you can use Python at a lot of different places!\n',check_concat)
+if __name__ == '__main__':
+	unittest.main()
