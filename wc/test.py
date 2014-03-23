953db174e73a888169897d068465ed5fd1abfd49
feralieva <t.feralieva@gmail.com>
2014-03-23 08:32:55 +0200

    wc

---
+++ wc/test.py
@@ -0,0 +1,20 @@
+import wc
+import unittest
+from solution import check_output
+
+class TestWc(unittest.TestCase):
+	"""docstring for TestWc"""
+	def test_wc_words(self):
+		words = check_output("py wc.py story.txt words",shell=True).decode()
+		self.assertEqual('163\n',words)
+
+	def test_wc_lines(self):
+		lines = check_output("py wc.py story.txt lines",shell=True).decode()
+		self.assertEqual('4\n',lines)
+
+	def test_wc_chars(self):
+		chars = check_output("py wc.py story.txt chars",shell=True).decode()
+		self.assertEqual('1031\n',chars)
+
+if __name__ == '__main__':
+	unittest.main()
