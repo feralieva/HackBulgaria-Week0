7943c45328c98a69fd64b05b01f519c4e7171465
feralieva <t.feralieva@gmail.com>
2014-03-23 08:29:49 +0200

    sort fractions

---
+++ sort_fract/test.py
@@ -0,0 +1,12 @@
+from solution import sort_fractions
+import unittest
+
+class TestSortFractions(unittest.TestCase):
+	"""docstring for TestSortFractions"""
+	def test_sort_fractions(self):
+		self.assertEqual([(1, 2), (2, 3)],sort_fractions([(2, 3), (1, 2)]))
+		self.assertEqual([(1, 3), (1, 2), (2, 3)],sort_fractions([(2, 3), (1, 2), (1, 3)]))
+		self.assertEqual([(22, 78), (15, 32), (5, 6), (7, 8), (9, 6), (22, 7)],sort_fractions([(5, 6), (22, 78), (22, 7), (7, 8), (9, 6), (15, 32)]))
+
+if __name__ == '__main__':
+	unittest.main()
